cc07bc01f6f2af6079bb65f5875bae89c507cf47
Blandaren123 <toskha_91@hotmail.se>
2021-12-04 18:59:17 +0000

    add container list for each category

--- run.py
+++ run.py
@@ -1,2 +1,5 @@
 import random, time
+marvelHeroes = ['thor', 'loki', 'vision', 'deadpool', 'hawkeye', 'mantis', 'antman']
+dcHeroes = ['batman', 'superman', 'flash', 'cyborg', 'robin', 'aquaman', 'joker']
+
 
