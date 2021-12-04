c16b51b40a427a451b251969a623092b36530a64
Blandaren123 <toskha_91@hotmail.se>
2021-12-04 19:21:23 +0000

    add choice for the 2 list

--- run.py
+++ run.py
@@ -8,7 +8,20 @@ continueGame = "Y"
 name = input("Enter your Gametag:")
 print("Hello and Welcome", name.capitalize(), "to play Hangman game!!")
 time.sleep(1)
-print("You must guess the secret word chosen by the computer to win the game.")
+print("You must guess the word chosen by the computer to win the game.")
 time.sleep(3)
 print("Each guess may be only one letter. Remember to press 'enter' after each guess.")
+time.sleep(3)
+print("Good luck and have fun!")
+time.sleep(2)
 
+while True:
+    while True:
+        if category.upper() == 'M':
+            wordList = random.choice(marvelHeroes)
+            break
+        elif category.upper() == 'D':
+            wordList = random.choice(dcHeroes)
+            break
+
+    
