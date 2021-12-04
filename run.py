9b0294405984e8597ff607756857508994710904
Blandaren123 <toskha_91@hotmail.se>
2021-12-04 19:11:54 +0000

    add name input and game rules for the game

--- run.py
+++ run.py
@@ -1,5 +1,14 @@
 import random, time
 marvelHeroes = ['thor', 'loki', 'vision', 'deadpool', 'hawkeye', 'mantis', 'antman']
 dcHeroes = ['batman', 'superman', 'flash', 'cyborg', 'robin', 'aquaman', 'joker']
+playGame = True
+category = ""
+continueGame = "Y"
 
+name = input("Enter your Gametag:")
+print("Hello and Welcome", name.capitalize(), "to play Hangman game!!")
+time.sleep(1)
+print("You must guess the secret word chosen by the computer to win the game.")
+time.sleep(3)
+print("Each guess may be only one letter. Remember to press 'enter' after each guess.")
 
